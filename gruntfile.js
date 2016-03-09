46309ed83b2da6081d974971717bd8f469fb514a
guntidheerajkumar@gmail.com <guntidheerajkumar@gmail.com>
2016-03-09 18:22:24 +0530

    Main Project

---
+++ gruntfile.js
@@ -0,0 +1,96 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+  
+  grunt.initConfig({
+
+    // Set this variables for different projects
+    projectName: 'GitPublishSolution', // the name of the solution (without *.sln)
+    testProjectName: 'SampleTestProject', // the name of the test project (without *.csproj)
+
+    // These variables shouldn't be changed, but sometimes it might be necessary
+    srcPath: './',
+    solutionName: '<%= projectName %>.sln',
+    platform: 'Any CPU',
+    styleCopRules: 'Settings.StyleCop',
+    ruleSet: 'rules.ruleset',
+    githooks: {
+        all: {
+        // Will run the jshint and test:unit tasks at every commit 
+        'pre-commit': 'test',
+        }
+    },
+    pkg: grunt.file.readJSON('package.json'),
+    assemblyVersion: grunt.file.readJSON('package.json').version.replace(/\-[a-z]+/g,""), //version to remove the 'alpha'-like tags from the semver version
+
+    assemblyinfo: {
+      options: {
+        files: ['<%= srcPath %><%= solutionName %>'],
+        info: {
+          version: '<%= assemblyVersion %>',
+          fileVersion: '<%= pkg.version %>',
+          company: 'EPAM Systems',
+          copyright: ' � EPAM Systems',
+          product: '<%= projectName %>'
+        }
+      }
+    },
+
+    msbuild: {
+      release: {
+        src: ['<%= srcPath %><%= solutionName %>'],
+        options: {
+          projectConfiguration: 'Release',
+          platform: '<%= platform %>',
+          targets: ['Clean', 'Rebuild'],
+          buildParameters: {
+            StyleCopEnabled: false
+          }
+        }
+      },
+      debug: {
+        src: ['<%= srcPath %><%= solutionName %>'],
+        options: {
+          projectConfiguration: 'Debug',
+          platform: '<%= platform %>',
+          targets: ['Clean', 'Rebuild'],
+          buildParameters: {
+            StyleCopEnabled: true,
+            StyleCopTreatErrorsAsWarnings: false,
+            StyleCopOverrideSettingsFile: process.cwd() + '/<%= styleCopRules %>',
+            RunCodeAnalysis: true,
+            CodeAnalysisRuleSet: process.cwd() + '/<%= ruleSet %>',
+            TreatWarningsAsErrors: true
+          },
+        }
+      }
+    },
+
+    mstest: {
+      debug: {
+        src: ['<%= srcPath %>/<%= testProjectName %>/bin/Debug/<%= testProjectName %>.dll'] // Points to test dll
+      }
+    },
+
+    nugetrestore: {
+      restore: {
+        src: '<%= srcPath %>/<%= solutionName %>',
+        dest: '<%= srcPath %>/packages/'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-githooks'); 
+
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['nugetrestore','msbuild:release']);
+  grunt.registerTask('test', ['nugetrestore','msbuild:debug', 'mstest']);
+  grunt.registerTask('release', ['assemblyinfo', 'test']);
+};
